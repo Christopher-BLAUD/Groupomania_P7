ab6727894af20354a1d3f661f76cf3b60ad0e82c
Christopher Blaud <christopher.blaud.esi@gmail.com>
2022-04-18 19:24:00 +0200

    modify .env

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize("groupo", "root", "ouhlala.2014", {
+/* const sequelize = new Sequelize("groupo", "root", "ouhlala.2014", {
+    dialect: "mysql",
+    host: "localhost",
+}); */
+
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
     dialect: "mysql",
     host: "localhost",
 });
